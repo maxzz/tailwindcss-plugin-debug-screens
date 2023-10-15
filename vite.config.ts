353165256de259f75aa9cd29e3be5c28956ff856
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-14 18:52:36 -0700

    WIP. add vite lib build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
+    build: {
+        target: "ES2019",
+        lib: {
+            entry: resolve(__dirname, 'tailwind/tailwind-plugin-debug-screens.ts'),
+            name: 'react-tailwindcss-debug-screens',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `plugin/index.${format}.${format === 'es' ? 'm' : ''}js`,
+        },
+        minify: false,
+        rollupOptions: {
+            treeshake: true,
+            external: [
+                'tailwindcss/plugin',
+                'tailwindcss/defaultTheme',
+                'tailwindcss/types/config',
+            ],
+            output: {
+                globals: {
+                    tailwindcss: 'tailwindcss',
+                },
+            },
+        },
+    },
     plugins: [
         react()
     ],
